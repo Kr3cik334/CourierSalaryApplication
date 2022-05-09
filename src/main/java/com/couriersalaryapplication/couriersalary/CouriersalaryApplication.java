0b04282d807c3c4be0a8c13c40ec57762c601618
Kr3cik334 <92583232+Kr3cik334@users.noreply.github.com>
2022-05-09 15:05:49 +0200

    Add initial files

---
+++ src/main/java/com/couriersalaryapplication/couriersalary/CouriersalaryApplication.java
@@ -0,0 +1,13 @@
+package com.couriersalaryapplication.couriersalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouriersalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouriersalaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couriersalaryapplication/couriersalary/CouriersalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couriersalaryapplication.couriersalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouriersalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
